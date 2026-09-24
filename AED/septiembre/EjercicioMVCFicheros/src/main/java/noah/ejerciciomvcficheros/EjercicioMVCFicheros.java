9b33a1e577b935e4043e6321826d270685702f16
Noah2damb <noah2dam@gmail.com>
2026-09-24 18:17:23 +0100

    new: ejercicio aplicando mvc

---
+++ AED/septiembre/EjercicioMVCFicheros/src/main/java/noah/ejerciciomvcficheros/EjercicioMVCFicheros.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package noah.ejerciciomvcficheros;
+
+/**
+ *
+ * @author 2damb
+ */
+public class EjercicioMVCFicheros {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
